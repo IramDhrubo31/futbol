83f61e16f622366a6846bbf75c90c107a843755a
sumaiyaakternitu924 <sumaiyaakternitu924@gmail.com>
2023-12-09 20:16:12 +0000

    Update Contact Page

---
+++ Contact_Page/script.js
@@ -0,0 +1,8 @@
+function openPopup() {
+    document.getElementById('contact-popup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('contact-popup').style.display = 'none';
+}
+
