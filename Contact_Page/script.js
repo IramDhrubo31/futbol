c44188e2967302f2f3d94586b59715f51c70a90c
sumaiyaakternitu924 <sumaiyaakternitu924@gmail.com>
2023-12-10 21:45:40 +0000

    Contact list add

--- Contact_Page/script.js
+++ Contact_Page/script.js
@@ -6,3 +6,18 @@ function closePopup() {
     document.getElementById('contact-popup').style.display = 'none';
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); 
+        var confirmationMessage = document.getElementById('confirmation-message');
+        confirmationMessage.innerText = 'Your Form is Submitted';
+        confirmationMessage.style.display = 'block';
+
+        
+        closePopup();
+    });
+});
+
+
