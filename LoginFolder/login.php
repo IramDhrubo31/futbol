1e0170f0303a5d6250cdeb1d1aa59eeb722a415b
sumaiyaakternitu924 <2022350@iub.edu.bd>
2023-12-13 16:08:53 +0000

    Add login PHP page

--- LoginFolder/login.php
+++ LoginFolder/login.php
@@ -1,11 +1,33 @@
 <?php
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     session_start();
     $name = $_POST['name'];
     $pass = $_POST['pass'];
-    if ($name=='admin' && $pass='3333'){
-        $_SESSION['name']=$name;
-        header('location:contactlist.php');
+
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "futbol";
+
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
+
+
+    $sql = "SELECT * FROM user_table WHERE user_name = '$name' AND password = '$pass'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['name'] = $name;
+        header('location: pdashboard.html');
+    } else {
+        echo "Login unsuccessful. Please check your username and password.";
+    }
+
+    $conn->close();
 }
-?>
