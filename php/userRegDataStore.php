ce9461f7723d3d7466488013e5454006f8619551
AnikaTabassumm <2022335@iub.edu.bd>
2023-12-13 21:28:21 +0600

    php thik kor

--- php/userRegDataStore.php
+++ php/userRegDataStore.php
@@ -1,19 +1,21 @@
 <?php
 if(isset($_POST['submit'])) {
-    $name=$_POST['name'];
+    $fname=$_POST['namee'];
     $uname=$_POST['userName'];
     $email=$_POST['email'];
     $contact=$_POST['contactNumber'];
-    $password=$_POST['password'];
+    $upassword=$_POST['password'];
     $gender=$_POST['gender'];
     $bloodGroup=$_POST['bloodGroup'];
     $profileImage=$_POST['profileImage'];
+    $c_password=$_POST['c_password'];
 
-    echo $name;
+
+    echo $fname;
     echo $uname;
     echo $email;
     echo $contact;
-    echo $password;
+    echo $upassword;
     echo $gender;
     echo $bloodGroup;
     echo $profileImage;
@@ -24,13 +26,29 @@ if(isset($_POST['submit'])) {
         die("ERROR: Could not connect. ". mysqli_connect_error());
     }
 
-    $sql = "INSERT INTO contact_info(firstName, lastName, email, phoneNumber, messages) VALUES ('$fname', '$lname', '$email', '$phone', '$message')";
-    if(mysqli_query($link, $sql)) {
-        echo "Records added successfully.";
+    $sql = "INSERT INTO user_table(name, user_name, email, contactNumber, password, gender, bloodGroup, picture) VALUES ('$fname', '$uname', '$email', '$contact', '$upassword', '$gender, '$bloodGroup', '$profileImage')";
+
+    if ($upassword == $c_password) {
+        echo "Passwords match!";
+        if(mysqli_query($link, $sql)) {
+            echo "Records added successfully.";
+        }
+        else {
+            echo "ERROR: Could not able to execute $sql." . mysqli_error($link);
+        }
+       
+    } else {
+        echo "Passwords do not match. Please try again.";
+        header('location: ../LoginFolder/signUp.html');
     }
-    else {
-        echo "ERROR: Could not able to execute $sql." . mysqli_error($link);
-    }
+
+    // if(mysqli_query($link, $sql)) {
+    //     echo "Records added successfully.";
+    // }
+    // else {
+    //     echo "ERROR: Could not able to execute $sql." . mysqli_error($link);
+    // }
+
 
     mysqli_close($link);
 }
