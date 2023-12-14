fa8405c1989f13b7481d57b67dededbffb0aa29b
AnikaTabassumm <2022335@iub.edu.bd>
2023-12-14 22:26:01 +0600

    Dashboard

--- dashboard.php
+++ dashboard.php
@@ -21,7 +21,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/pdashboard.css">
+    <link rel="stylesheet" href="css/dashboard.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title>Dashboard</title>
