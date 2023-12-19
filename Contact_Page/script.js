b2a47d13e995cf0c76e828fd0b5f428be78ac799
sumaiyaakternitu924 <2022350@iub.edu.bd>
2023-12-19 05:23:23 +0000

    Update

--- Contact_Page/script.js
+++ Contact_Page/script.js
@@ -1,29 +1,25 @@
-function openPopup() {
-    document.getElementById('contact-popup').style.display = 'block';
+function openPopup(){
+    document.getElementById('contact-popup').style.display = 'block'; 
 }
-
-function closePopup() {
-    document.getElementById('contact-popup').style.display = 'none';
+function closePopup(){
+    document.getElementById('contact-popup').style.display = 'none'; 
 }
+ 
+function calculate(){
+    event.preventDefault(); 
 
-function calculate() {
-    event.preventDefault();
+    var userName = document.getElementById("userName").value; 
+    var contactNumber = document.getElementById("contactNumber").value; 
+    var Email = document.getElementById("email").value; 
+    var message = document.getElementById("message").value; 
 
-    var userName = document.getElementById("userName").value;
-    var contactNumber = document.getElementById("contactNumber").value;
-    var Email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-
-    if (userName === "" || contactNumber === "" || Email === "" || message === "") {
+    if (userName === "" || contactNumber === "" || Email === "" || message === ""){
         document.getElementById("content").innerHTML = "<h4>Your registration is error. Please fill in all the required fields.</h4>";
-
     }
-
-    else {
-        document.getElementById("content").innerHTML = `<h4>Thank you for reaching out! Your message has been received, and we'll get back to you shortly.</h4>`;
-    }
-
-}
+    else{ 
+        document.getElementById("content").innerHTML = `<h4>Thank you for reaching out! Your message has been received, and we'll get back to you shortly.</h4>`; 
+    } 
+} 
 
 
 
